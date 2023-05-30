fcc69599c0d4fed85526a7a2c8e0e941faf7b382
Camillus Cai <cgcai@qxcg.net>
2023-05-29 17:30:44 -0700

    Set 400 steps/mm for MicroSwissNG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1171,7 +1171,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
